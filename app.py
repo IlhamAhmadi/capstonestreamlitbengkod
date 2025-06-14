fd0543c41cbb331aee0dd0cd1e913fa0510a252a
IlhamAhmadi <128119869+IlhamAhmadi@users.noreply.github.com>
2025-06-15 00:58:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 st.set_page_config(page_title="Prediksi Obesitas", layout="centered")
 
-st.title("Prediksi Tingkat Obesitas")
-st.markdown("Masukkan data berikut untuk memprediksi tingkat obesitas berdasarkan kebiasaan harian Anda.")
+st.title("Mari Kita Prediksi Tingkat Obesitasmu")
+st.markdown("Nah kamu wajib ngisi data dibawah ini biar bisa tauu")
 
 try:
     with open("model_bagus.pkl", "rb") as f:
@@ -18,14 +18,14 @@ except Exception as e:
 
 # Label encoders
 label_maps = {
-    'Gender': ['Female', 'Male'],
-    'family_history_with_overweight': ['no', 'yes'],
-    'FAVC': ['no', 'yes'],
-    'CAEC': ['no', 'Sometimes', 'Frequently', 'Always'],
-    'SMOKE': ['no', 'yes'],
-    'SCC': ['no', 'yes'],
-    'CALC': ['no', 'Sometimes', 'Frequently', 'Always'],
-    'MTRANS': ['Automobile', 'Bike', 'Motorbike', 'Public_Transportation', 'Walking']
+    'Gender': ['Cewek', 'Cowok'],
+    'family_history_with_overweight': ['Iya', 'Enggak'],
+    'FAVC': ['Iya', 'Enggak'],
+    'CAEC': ['Enggak', 'Kadang sih', 'Sering hehe', 'Selaluuuu'],
+    'SMOKE': ['Anti rokok gess', 'Woiya jelass harus perasapan'],
+    'SCC': ['Enggak', 'Iya'],
+    'CALC': ['Enggak', 'Kadang sih', 'Sering hehe', 'Selaluuuu'],
+    'MTRANS': ['Mobill', 'Sepeda', 'Motoran', 'Transportasi publik (BRT contohnya)', 'Anak sehat jalan kaki']
 }
 
 def encode_column(value, classes):
@@ -38,18 +38,18 @@ gender = st.selectbox('Jenis Kelamin', label_maps['Gender'])
 age = st.slider('Usia', 10, 100, 25)
 height = st.slider('Tinggi Badan (m)', 1.3, 2.2, 1.7)
 weight = st.slider('Berat Badan (kg)', 30, 200, 70)
-family_history = st.selectbox('Riwayat keluarga overweight?', label_maps['family_history_with_overweight'])
+family_history = st.selectbox('Ada keluarga yg obesitas?', label_maps['family_history_with_overweight'])
 favc = st.selectbox('Sering makan makanan tinggi kalori?', label_maps['FAVC'])
-fcvc = st.slider('Frekuensi konsumsi sayur (1-3)', 1, 3, 2)
-ncp = st.slider('Jumlah makan besar per hari (1-4)', 1, 4, 3)
+fcvc = st.slider('Sering makan sayur gak? (1-3)', 1, 3, 2)
+ncp = st.slider('Jumlah makan besar /hari berapa banyak? (1-4)', 1, 4, 3)
 caec = st.selectbox('Ngemil?', label_maps['CAEC'])
 smoke = st.selectbox('Merokok?', label_maps['SMOKE'])
-scc = st.selectbox('Kontrol kalori?', label_maps['SCC'])
-ch2o = st.slider('Konsumsi air harian (1-3 liter)', 1, 3, 2)
-faf = st.slider('Frekuensi aktivitas fisik (0-3)', 0, 3, 1)
-tue = st.slider('Waktu menatap layar/hari (0-2 jam)', 0, 2, 1)
+scc = st.selectbox('Kontrol kalori gak?', label_maps['SCC'])
+ch2o = st.slider('Konsumsi air harian berapa banyak? (1-3 liter)', 1, 3, 2)
+faf = st.slider('Frekuensi aktivitas fisiknya seberapa sering? (0-3)', 0, 3, 1)
+tue = st.slider('Waktu menatap layar /hari (0-2 jam)', 0, 2, 1)
 calc = st.selectbox('Konsumsi alkohol?', label_maps['CALC'])
-mtrans = st.selectbox('Transportasi utama?', label_maps['MTRANS'])
+mtrans = st.selectbox('Transportasi utamanya apa?', label_maps['MTRANS'])
 
 # Encode semua data
 input_data = {
